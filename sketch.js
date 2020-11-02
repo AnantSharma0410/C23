93c3a4eed9533d22d22c09064cafa00afadef554
AnantSharma0410 <72499139+AnantSharma0410@users.noreply.github.com>
2020-11-02 19:53:20 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -65,12 +65,23 @@ function draw() {
  
 }
 
-function keyPressed() {
- if (keyCode === DOWN_ARROW) {Matter.Body.setStatic(packageBody, false);}
-}
 
 function stopbox(){
   if(numberoftimeboxbounce>=2){
     Matter.Body.setStatic(packageBody, true);
   }
+}
+function keyPressed() { 
+	if (keyCode === LEFT_ARROW) { 
+		helicopterSprite.x=helicopterSprite.x-20; 
+		translation={x:-20,y:0} 
+		Matter.Body.translate(packageBody, translation) 
+	} 
+	else if (keyCode === RIGHT_ARROW) { 
+	helicopterSprite.x=helicopterSprite.x+20; 
+	translation={x:20,y:0} 
+Matter.Body.translate(packageBody, translation) } 
+else if (keyCode === DOWN_ARROW) { 
+	Matter.Body.setStatic(packageBody,false); 
+} 
 }
